0497f367f3e47fb6056fc22f3a3590ece3c06c63
linxsal <fgramajo00@gmail.com>
2024-02-05 22:14:10 -0300

    Update homework.js

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -86,6 +86,8 @@ function promedioResultadosTest(resultadosTest) {
    let suma = 0;
    for (let i=0;i<resultadosTest.length;i){
       suma += resultadosTest[i];
+      const promedio = sumaNotas / resultadosTest.length;
+      return promedio;
    }
 }
 
